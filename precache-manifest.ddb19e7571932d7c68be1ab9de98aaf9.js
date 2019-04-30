f747c8704e0257640fcfe17841394c7d31657203
lbssone <loveseo1124@gmail.com>
2019-05-01 02:05:09 +0800

    Updates

---
+++ precache-manifest.ddb19e7571932d7c68be1ab9de98aaf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "574e65ffb71ac9f68d4f",
+    "url": "/new_todo/static/js/runtime~main.c309b8d8.js"
+  },
+  {
+    "revision": "9b20a9920d7be978112d",
+    "url": "/new_todo/static/js/main.ac211d93.chunk.js"
+  },
+  {
+    "revision": "4556de45b53ce250c0a4",
+    "url": "/new_todo/static/js/2.fe1dce63.chunk.js"
+  },
+  {
+    "revision": "9b20a9920d7be978112d",
+    "url": "/new_todo/static/css/main.c417ee44.chunk.css"
+  },
+  {
+    "revision": "ae5a5e5f94cff66f6955f120ddc9ae2f",
+    "url": "/new_todo/index.html"
+  }
+];
